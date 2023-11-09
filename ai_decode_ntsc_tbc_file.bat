4ab86d66148a8309a1594eaf6c2b64888d5588b3
vrunk11 <56720403+vrunk11@users.noreply.github.com>
2023-11-09 20:57:32 +0100

    Move script + add model
    add folder containing model  and move script to a specific folder

--- ai_decode_ntsc_tbc_file.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "%~dp1%~n1.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "%~dp0/cvbs_ai_decode.exe" -m %~dp0/ntsc_tbc -o tbc -t 4 | ffmpeg -hide_banner -y -f rawvideo -pix_fmt y16 -video_size 910x242 -r (30000/1001)*2 -i - -f rawvideo -video_size 910x263 -pix_fmt gray16 -r (30000/1001)*2 -i "%~dp1%~n1.tbc" -filter_complex "[1][0]overlay=0:20" -pix_fmt y16 -f rawvideo -c:v rawvideo "%~dp1%~n1_aitbc.tbc"
-pause
