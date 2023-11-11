a9d8c5a6dbb226c3c11e236db0b66f7dd5b326fd
vrunk11 <56720403+vrunk11@users.noreply.github.com>
2023-11-11 15:28:12 +0100

    fix script
    fix script not using good path for the model

--- script/linux/ai_decode_ntsc_tbc_file.sh
+++ script/linux/ai_decode_ntsc_tbc_file.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
 cd ../../
-ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "$(dirname $1)/$(basename -s $1).tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "cvbs_ai_decode" -m $pwd/ntsc_decode -o tbc -t 4 | ffmpeg -hide_banner -y -f rawvideo -pix_fmt y16 -video_size 910x242 -r (30000/1001)*2 -i - -f rawvideo -video_size 910x263 -pix_fmt gray16 -r (30000/1001)*2 -i "$(dirname $1)/$(basename -s $1).tbc" -filter_complex "[1][0]overlay=0:20" -pix_fmt y16 -f rawvideo -c:v rawvideo "$(dirname $1)/$(basename -s $1)_aitbc.tbc"
+ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "$(dirname $1)/$(basename -s $1).tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "cvbs_ai_decode" -m $pwd/model/ntsc_decode -o tbc -t 4 | ffmpeg -hide_banner -y -f rawvideo -pix_fmt y16 -video_size 910x242 -r (30000/1001)*2 -i - -f rawvideo -video_size 910x263 -pix_fmt gray16 -r (30000/1001)*2 -i "$(dirname $1)/$(basename -s $1).tbc" -filter_complex "[1][0]overlay=0:20" -pix_fmt y16 -f rawvideo -c:v rawvideo "$(dirname $1)/$(basename -s $1)_aitbc.tbc"
 read -n 1 -s -r -p "Press any key to continue"
--- script/windows/ai_decode_ntsc_file.bat
+++ script/windows/ai_decode_ntsc_file.bat
@@ -2,5 +2,5 @@
 %~d0
 cd %~dp0
 cd ../../
-ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "%~dp1%~n1.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "cvbs_ai_decode.exe" -m %CD%/ntsc_1d -t 4 | ffmpeg -hide_banner -y -f rawvideo -r (30000/1001) -video_size 792x484 -pix_fmt rgb48 -i - -filter_complex "extractplanes=r+g+b[Ry][Gu][Bv];[Ry][Gu][Bv]mergeplanes=0x001020:yuv444p16,format=yuv422p,il=l=i:c=i:a=i,setdar=4/3,crop=760:484:16:0" -c:v ffv1 -coder 1 -context 0 -level 3 -slices 4 -slicecrc 0 -pass 1 -top 1 "%~dp1%~n1_aidecode.mkv"
+ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "%~dp1%~n1.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "cvbs_ai_decode.exe" -m %CD%/model/ntsc_1d -t 4 | ffmpeg -hide_banner -y -f rawvideo -r (30000/1001) -video_size 792x484 -pix_fmt rgb48 -i - -filter_complex "extractplanes=r+g+b[Ry][Gu][Bv];[Ry][Gu][Bv]mergeplanes=0x001020:yuv444p16,format=yuv422p,il=l=i:c=i:a=i,setdar=4/3,crop=760:484:16:0" -c:v ffv1 -coder 1 -context 0 -level 3 -slices 4 -slicecrc 0 -pass 1 -top 1 "%~dp1%~n1_aidecode.mkv"
 pause
