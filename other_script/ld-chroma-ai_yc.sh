3b80b0fa675ea6f6c7b58ece8c3b12baf5fb4bb0
vrunk11 <56720403+vrunk11@users.noreply.github.com>
2023-12-02 10:36:31 +0100

    small fix
    add copy of the .json file on some script
    add option in the ld-chroma-ai_yc.sh script
    set linux script as runable

--- other_script/ld-chroma-ai_yc.sh
+++ other_script/ld-chroma-ai_yc.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 cd $(dirname $0)
 FILENAME=$(basename "$1" .tbc)
+
+echo chroma gain (1): && read CHROMAGAIN
+echo chroma phase (1): && read CHROMAPHASE
+echo NTSC phase compensation (1): && read PHASECOMP
+
+if [$CHROMAGAIN != "0" ] && [$CHROMAGAIN != "1" ]; then $CHROMAGAIN = "1"; fi
+if [$CHROMAPHASE != "0" ] && [$CHROMAPHASE != "1" ]; then $CHROMAPHASE = "1"; fi
+if [$PHASECOMP != "0" ] && [$PHASECOMP != "1" ]; then $PHASECOMP = "1"; fi
+if [$PHASECOMP == "1" ]; then $PHASECOMP = "--ntsc-phase-comp"; else $PHASECOMP = "" fi
+
 mkfifo lumapipe
 ld-chroma-decoder $FILENAME --input-json "$(dirname $1)/$FILENAME.json" -f ntsc3d --luma-nr 0 --chroma-nr 0 --chroma-gain 0 --chroma-phase 1 -p y4m | ffmpeg -y -i - -c:v rawvideo -f nut pipe:1 > lumapipe
-ld-chroma-decoder "$(dirname $1)/$FILENAME_chroma.tbc" --input-json "$(dirname $1)/$FILENAME.json" -f ntsc2d --ntsc-phase-comp --luma-nr 0 --chroma-nr 0 --chroma-gain 1 --chroma-phase 0 -p y4m | ffmpeg -y -r "(30000/1001)*2" -i lumapipe -r "(30000/1001)*2" -i - -filter_complex "[0]format=pix_fmts=yuv444p,extractplanes=y[y]; [1]format=pix_fmts=yuv444p,extractplanes=u+v[u][v]; [y][u][v]mergeplanes=0x001020:yuv444p,format=pix_fmts=yuv444p" -r "(30000/1001)*2" -top 1 -c:v ffv1 -coder 1 -context 0 -level 3 -slices 4 -slicecrc 0 -pass 1 -c:a flac -compression_level 11 "$(dirname $1)/$FILENAME.mkv"
+ld-chroma-decoder "$(dirname $1)/$FILENAME_chroma.tbc" --input-json "$(dirname $1)/$FILENAME.json" -f ntsc2d --ntsc-phase-comp --luma-nr 0 --chroma-nr 0 --chroma-gain $CHROMAGAIN --chroma-phase $CHROMAPHASE -p y4m | ffmpeg -y -r "(30000/1001)*2" -i lumapipe -r "(30000/1001)*2" -i - -filter_complex "[0]format=pix_fmts=yuv444p,extractplanes=y[y]; [1]format=pix_fmts=yuv444p,extractplanes=u+v[u][v]; [y][u][v]mergeplanes=0x001020:yuv444p,format=pix_fmts=yuv444p" -r "(30000/1001)*2" -top 1 -c:v ffv1 -coder 1 -context 0 -level 3 -slices 4 -slicecrc 0 -pass 1 -c:a flac -compression_level 11 "$(dirname $1)/$FILENAME.mkv"
 echo end of the script
 read -n 1 -s -r -p "Press any key to continue"
--- script/linux/ai_decode_ntsc_tbc_file.sh
+++ script/linux/ai_decode_ntsc_tbc_file.sh
@@ -3,4 +3,5 @@ cd $(dirname $0)
 cd ../../
 FILENAME=$(basename "$1" .tbc)
 ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "$(dirname $1)/$FILENAME.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | /lib64/ld-linux-x86-64.so.2 --library-path "./" "./cvbs_ai_decode" -m $(pwd)/model/ntsc_decode -o tbc -t 4 | ffmpeg -hide_banner -y -f rawvideo -pix_fmt y16 -video_size 910x242 -r "(30000/1001)*2" -i - -f rawvideo -video_size 910x263 -pix_fmt gray16 -r "(30000/1001)*2" -i "$(dirname $1)/$FILENAME.tbc" -filter_complex "[1][0]overlay=0:20" -pix_fmt y16 -f rawvideo -c:v rawvideo "$(dirname $1)/$FILENAME_aitbc.tbc"
+cp $FILENAME.tbc.json $FILENAME_aitbc.tbc.json
 read -n 1 -s -r -p "Press any key to continue"
