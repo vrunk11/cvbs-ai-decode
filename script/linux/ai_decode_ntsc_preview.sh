2791543fccf11bf6045d56ecb4fb4ddc4c2bc6da
vrunk11 <56720403+vrunk11@users.noreply.github.com>
2023-12-02 11:22:51 +0100

    Add thread_queue_size in ffmpeg command
    Add -thread_queue_size 4096 in ffmpeg command

--- script/linux/ai_decode_ntsc_preview.sh
+++ script/linux/ai_decode_ntsc_preview.sh
@@ -2,5 +2,5 @@
 cd $(dirname $0)
 cd ../../
 FILENAME=$(basename "$1" .tbc)
-ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "$(dirname $1)/$FILENAME.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | /lib64/ld-linux-x86-64.so.2 --library-path "./" "./cvbs_ai_decode" -m $(pwd)/model/ntsc_decode -t 4 | ffmpeg -hide_banner -loglevel error -f rawvideo -r "(30000/1001)" -video_size 792x484 -pix_fmt rgb48 -i - -filter_complex "extractplanes=r+g+b[Ry][Gu][Bv];[Ry][Gu][Bv]mergeplanes=0x001020:yuv444p16,format=yuv444p16,il=l=i:c=i:a=i,setdar=4/3" -f nut -c:v rawvideo - | ffplay -hide_banner -i - -vf crop=760:484:16:0,w3fdif
+ffmpeg -thread_queue_size 4096 -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "$(dirname $1)/$FILENAME.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | /lib64/ld-linux-x86-64.so.2 --library-path "./" "./cvbs_ai_decode" -m $(pwd)/model/ntsc_decode -t 4 | ffmpeg -thread_queue_size 4096 -hide_banner -loglevel error -f rawvideo -r "(30000/1001)" -video_size 792x484 -pix_fmt rgb48 -i - -filter_complex "extractplanes=r+g+b[Ry][Gu][Bv];[Ry][Gu][Bv]mergeplanes=0x001020:yuv444p16,format=yuv444p16,il=l=i:c=i:a=i,setdar=4/3" -f nut -c:v rawvideo - | ffplay -hide_banner -i - -vf crop=760:484:16:0,w3fdif
 read -n 1 -s -r -p "Press any key to continue"
--- script/windows/ai_decode_ntsc_preview.bat
+++ script/windows/ai_decode_ntsc_preview.bat
@@ -2,5 +2,5 @@
 %~d0
 cd %~dp0
 cd ../../
-ffmpeg -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "%~dp1%~n1.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "cvbs_ai_decode.exe" -m %CD%/model/ntsc_decode -t 4 | ffmpeg -hide_banner -loglevel error -f rawvideo -r (30000/1001) -video_size 792x484 -pix_fmt rgb48 -i - -filter_complex "extractplanes=r+g+b[Ry][Gu][Bv];[Ry][Gu][Bv]mergeplanes=0x001020:yuv444p16,format=yuv444p16,il=l=i:c=i:a=i,setdar=4/3" -f nut -c:v rawvideo - | ffplay -hide_banner -i - -vf crop=760:484:16:0,w3fdif
+ffmpeg -thread_queue_size 4096 -hide_banner -loglevel error -f rawvideo -video_size 910x263 -pix_fmt gray16 -i "%~dp1%~n1.tbc" -vf crop=910:242:0:20 -c:v rawvideo -f rawvideo - | "cvbs_ai_decode.exe" -m %CD%/model/ntsc_decode -t 4 | ffmpeg -thread_queue_size 4096 -hide_banner -loglevel error -f rawvideo -r (30000/1001) -video_size 792x484 -pix_fmt rgb48 -i - -filter_complex "extractplanes=r+g+b[Ry][Gu][Bv];[Ry][Gu][Bv]mergeplanes=0x001020:yuv444p16,format=yuv444p16,il=l=i:c=i:a=i,setdar=4/3" -f nut -c:v rawvideo - | ffplay -hide_banner -i - -vf crop=760:484:16:0,w3fdif
 pause
